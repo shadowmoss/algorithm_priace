26f36d610517e5c9f62d03434cf9b5454ef5c5ad
ltx <18284806010@163.com>
2022-09-30 11:27:49 +0800

    first commit

---
+++ src/main/java/ltx/StringBitCompute.java
@@ -0,0 +1,21 @@
+package ltx;
+
+/**
+ * @author ltx
+ * @description:
+ * @date 2022/9/20 12:12
+ */
+// bit位加法运算
+public class StringBitCompute {
+    public static void main(String[] args) {
+        // 思路将两个字符串拆解成字符数组
+        // 右端对齐,进行逐个运算
+        // 1、两边长度相同的情况
+        // 2、两边长度不同的情况
+        String first = "110011";
+        String second = "1111001";
+        char[] firstArray = first.toCharArray();
+        char[] secondArray = second.toCharArray();
+        System.out.println();
+    }
+}
